f82265b840f881ac439e24570354714af70c6521
Simon Wong <sw26wong@uwaterloo.ca>
2019-11-20 20:18:37 -0500

    misc files

--- Objects/rtos_semaphore.d
+++ Objects/rtos_semaphore.d
@@ -1,4 +1,4 @@
 .\objects\rtos_semaphore.o: rtos_semaphore.c
-.\objects\rtos_semaphore.o: C:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\rtos_semaphore.o: C:\Software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\rtos_semaphore.o: rtos_semaphore.h
 .\objects\rtos_semaphore.o: rtos_queue.h
